8b406bfb00ed6bb2550449098696010bfa6ee49d
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-22 16:31:42 +0200

    feat js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 console.log ("Primi passi col DOM")
 
+
+// Accendo la lampadina
+
+// Creo 2 variabili (una per l'immagine e una per il pulsante)
 const lamp = document.getElementById("lamp");
 const btn = document.getElementById("btn");
 
+// Dico al pulsante di cambiare immagine al click
 btn.addEventListener("click", function() {
   lamp.src = "./img/yellow_lamp.png";
 });
