10ab56378e9564eb55d64b53cce925c0668445c8
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-22 15:45:36 +0200

    add yellow lamp

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 console.log ("Primi passi col DOM")
 
+const lamp = document.getElementById("lamp");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+  lamp.src = "./img/yellow_lamp.png";
+});
